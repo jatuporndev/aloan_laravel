25a893c4b248a8a3d9cc8714e2b82aabfc84e6f5
rejecthumanityreturntopug <monzzz0002@gmail.com>
2021-07-27 02:19:02 +0700

    ดีป

--- resources/views/dashboard/admin/AdminBank.blade.php
+++ resources/views/dashboard/admin/AdminBank.blade.php
@@ -7,12 +7,12 @@
         <div class="header-body">
           <div class="row align-items-center py-4">
             <div class="col-lg-6 col-7">
-              <h6 class="h2 text-white d-inline-block mb-0">Article</h6>
+              <h6 class="h2 text-white d-inline-block mb-0">Bank Manage</h6>
               <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                 <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
                   <li class="breadcrumb-item"><a href="{{ route('admin.home') }}"><i class="fas fa-home"></i></a></li>
                   <li class="breadcrumb-item"><a href="{{ route('admin.home') }}">Dashboards</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">article</li>
+                  <li class="breadcrumb-item active" aria-current="page">Bank</li>
                 </ol>
               </nav>
             </div>
